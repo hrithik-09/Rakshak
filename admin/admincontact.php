0daf6e3a8d5054165bd9e74aa9a7f91438c88ddf
Hrithik <ranjan.hrithik09@gmail.com>
2023-01-15 22:43:45 +0530

    removed access of home page from entire admin panel

--- admin/admincontact.php
+++ admin/admincontact.php
@@ -52,12 +52,7 @@ if (isset($_GET['delete'])) {
             <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
                 <div class="position-sticky pt-3 sidebar-sticky">
                     <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="../index.php">
-                                <span data-feather="home" class="align-text-bottom"></span>
-                                Home Page
-                            </a>
-                        </li>
+                       
                         <li class="nav-item">
                             <a class="nav-link" aria-current="page" href="admindashboard.php">
                                 <span data-feather="file" class="align-text-bottom"></span>
